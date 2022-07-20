25ca394106b9c540561ef6249cc87dad9c502311
Xavier Dosada <francoxavierdosada@gmail.com>
2022-07-19 23:29:18 -0300

    Entrega N2 Menu - Navbar

---
+++ Entrega N2 Menu Ecommerce/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <button className='navBar_button'>Menú</button>
+        <button className='navBar_button'>Productos</button>
+        <button className='navBar_button'>Sobre Nosotros</button>
+        <button className='navBar_button'>Preguntas Frecuentes</button>
+    </div>
+  )
+}
+
+export default Navbar
